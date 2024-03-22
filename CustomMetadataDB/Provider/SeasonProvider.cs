93aaa3f2bc91ff64fbfe446cd46e7b00be89fa97
ArabCoders <admin@arabcoders.org>
2024-03-22 19:00:44 +0300

    Added Season Provider

---
+++ CustomMetadataDB/Provider/SeasonProvider.cs
@@ -0,0 +1,47 @@
+using CustomMetadataDB.Helpers;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Common.Net;
+using System.Text.Json;
+
+namespace CustomMetadataDB;
+public class SeasonProvider : IRemoteMetadataProvider<Season, SeasonInfo>
+{
+    protected readonly ILogger _logger;
+    public string Name => Constants.PLUGIN_NAME;
+
+    public SeasonProvider(ILogger logger)
+    {
+        _logger = logger;
+        Utils.Logger = logger;
+    }
+
+    public Task<MetadataResult<Season>> GetMetadata(SeasonInfo info, CancellationToken cancellationToken)
+    {
+        _logger.Debug($"CMD Season GetMetadata: {JsonSerializer.Serialize(info)}");
+
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var result = new MetadataResult<Season>
+        {
+            HasMetadata = true,
+            Item = new Season
+            {
+                Name = info.Name,
+                IndexNumber = info.IndexNumber
+            }
+        };
+
+        return Task.FromResult(result);
+    }
+
+    public Task<IEnumerable<RemoteSearchResult>> GetSearchResults(SeasonInfo searchInfo, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public Task<HttpResponseInfo> GetImageResponse(string url, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
